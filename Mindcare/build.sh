8edf90114a50c221713a6036f2f8c6ad760ad125
Roopesh519 <rsroopesh565@gmail.com>
2024-08-10 22:22:47 +0530

    udpated the build.sh

--- Mindcare/build.sh
+++ Mindcare/build.sh
@@ -1,17 +1,29 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#!/bin/bash
 
+# Install Python (this assumes Vercel has Ubuntu or Debian-like environment)
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip python3-venv
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install Python dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Navigate to the directory where Rasa and Django are located
+cd Mindcare/MindCareModal
 
-# Start Rasa server
+# Run the Rasa server
 rasa run --enable-api --cors "*" &
 
-# Start Rasa action server
+# Run the Rasa action server
 rasa run actions &
 
-# Start Django server
-python manage.py runserver 0.0.0.0:8000
+# Navigate to Django directory
+cd ../MindCare
+
+# Run the Django development server
+python manage.py runserver 0.0.0.0:$PORT
