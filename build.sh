7626e1e55d25aeba3ee7ea06cc3ac6d6d0e7dfb0
Roopesh519 <rsroopesh565@gmail.com>
2024-08-10 23:44:05 +0530

    Ensure build.sh is executable

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Python (this assumes Vercel has Ubuntu or Debian-like environment)
-apt-get update
-apt-get install -y python3 python3-pip python3-venv
+# apt-get update
+# apt-get install -y python3 python3-pip python3-venv
 
 # Create a virtual environment
 python3 -m venv venv
